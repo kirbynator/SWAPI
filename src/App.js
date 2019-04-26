661d3420c62c907d99baa4c6dcbecc538b904dde
Zachary Kirby <i.am.kirbynator@gmail.com>
2019-04-25 23:03:56 -0600

    fixed one number

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import People from "./components/people";
+import Home from "./components/Home";
 import NoMatch from "./components/NoMatch";
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={People} />
+    <Route exact path="/" component={Home} />
     <Route component={NoMatch} />
   </Switch>
 );
--- src/components/people.js
+++ src/components/people.js
@@ -1,58 +1,26 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Card, Image } from "semantic-ui-react";
 
 const Example = () => {
   const [people, setPeople] = useState([]);
-  const [planets, setPlanets] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://swapi.co/api/people/?page=1").then(res => {
-      setPeople(res.data.results);
-    });
+  useEffect(props => {
+    setPeople(props.p);
   }, []);
 
-  const profile = q => {
-    let p = q;
-
-    if (planets.filter(w => w.url === p.homeworld).length === 0) {
-      debugger;
-      axios
-        .get(p.homeworld)
-        .then(res => {
-          const x = [res.data, ...planets];
-          setPlanets(x);
-          let g = { ...res.data };
-          return (
-            <Card>
-              <Image src={`../charaters/${parseInt(p.url)}`} />
-              <Card.Content>
-                <Card.Header>{p.name}</Card.Header>
-                <Card.Description>{`${p.name}'s Home Planet Is ${
-                  g.name
-                }`}</Card.Description>
-              </Card.Content>
-            </Card>
-          );
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } else {
-      debugger;
-      let g = planets.filter(w => w.url == p.homeworld);
-      return (
-        <Card>
-          <Image src={`../charaters/${parseInt(p.url)}`} />
-          <Card.Content>
-            <Card.Header>{p.name}</Card.Header>
-            <Card.Description>{`${p.name}'s Home Planet Is ${
-              g[0].name
-            }`}</Card.Description>
-          </Card.Content>
-        </Card>
-      );
-    }
+  const profile = p => {
+    debugger;
+    return (
+      <Card>
+        <Image src={`../charaters/${parseInt(p.url)}`} />
+        <Card.Content>
+          <Card.Header>{p.name}</Card.Header>
+          <Card.Description>{`${p.name}'s Home Planet Is ${
+            p.home.name
+          }`}</Card.Description>
+        </Card.Content>
+      </Card>
+    );
   };
 
   return (
